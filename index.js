32996e0bb18f163c741f40ddbb880806da6337f1
Otoniel Guajardo <otonielguajardo@yahoo.com>
2022-08-19 15:20:06 +0000

    removed cors

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const cors = require('cors')
+const cors = require('cors');
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 const uuid = require('uuid');
@@ -134,7 +134,7 @@ const logoutHandler = (req, res) => {
 
 const app = express();
 
-app.use(cors());
+//app.use(cors());
 app.use(bodyParser.json());
 app.use(cookieParser());
 
